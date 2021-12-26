9f41c06a224bf0f114850e911ba43c1ee6e4162c
steffen.gemin <steffen.gemin@student.howest.be>
2021-12-26 13:52:52 +0100

    Add checks for info

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import serial
 
+supported_types = ["roll", "pitch", "lat", "long", "alt", "date", "time", "uuid"]
+
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
+    ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1000)
     ser.flush()
 
     while True:
+        dataset = {}
         if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
-            print(line)
+            if "Info" in line:
+                print(line)
+            else:
+                for i in line.split(' | '):
+                    item = i.split(": ")
+                    data_type = item[0]
+                    value = item[1].replace(" |", "")
+                    if data_type in supported_types:
+                        dataset[data_type] = value
+
+                print(dataset)
