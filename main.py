8fc09230568142a750c7099138460e019bd24ef6
steffen.gemin <steffen.gemin@student.howest.be>
2022-01-03 17:11:34 +0100

    Create loop function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import serial
 
 active_uuid = ""
 supported_types = ["roll", "pitch", "lat", "long", "alt", "date", "speed", "time", "uuid"]
+dataset = []
 
 
 def set_uuid(uuid):
@@ -11,22 +12,32 @@ def set_uuid(uuid):
         active_uuid = uuid.replace(" ", "-")
 
 
+def get_data_from_arduino():
+    data = {}
+    if ser.in_waiting > 0:
+        line = ser.readline().decode('utf-8').rstrip()
+        if "Info" in line:
+            print(line)
+        else:
+            for i in line.split(' | '):
+                item = i.split(": ")
+                data_type = item[0]
+                value = item[1].replace(" |", "")
+                if data_type in supported_types:
+                    if data_type == "uuid":
+                        set_uuid(data[data_type])
+                    else:
+                        data[data_type] = value
+            dataset.append(data)
+
+
+def loop():
+    get_data_from_arduino()
+    
+
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1000)
     ser.flush()
 
     while True:
-        dataset = {}
-        if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip()
-            if "Info" in line:
-                print(line)
-            else:
-                for i in line.split(' | '):
-                    item = i.split(": ")
-                    data_type = item[0]
-                    value = item[1].replace(" |", "")
-                    if data_type in supported_types:
-                        dataset[data_type] = value
-
-                print(dataset)
+        loop()
